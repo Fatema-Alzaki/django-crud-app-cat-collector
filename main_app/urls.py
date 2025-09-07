b6fa9944f97c436079b60d26153bb060609869a3
Fatema-Alzaki <fatimaalzaki242@gmail.com>
2025-09-07 14:40:21 +0300

    just checking

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+# main_app/urls.py
+
+from django.urls import path
+from . import views # Import views to connect routes to view functions
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat_index'),
+]
